66d39fa80a2415dde07a503006d82b48942ab4ed
joskoetsier <joskoetsier@gmail.com>
2013-04-10 19:27:30 +0100

    fix error

---
+++ bin/startworker.sh
@@ -0,0 +1,9 @@
+#!/usrls -al //bin/python
+
+import rapidomero.worker.eventloop
+import sys
+
+argument = None
+if len(sys.argv)==2:
+    argument = sys.argv[1]
+rapidomero.worker.eventloop.AMQPLoop.start(argument)
